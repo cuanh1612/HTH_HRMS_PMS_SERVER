47d1bfee61bcec63ffbc55bdca7de927eafcc523
ngtientrong <trongtiens96@gmail.com>
2022-05-12 20:10:45 +0700

    fix add CRUD API Project

--- src/entities/Project.ts
+++ src/entities/Project.ts
@@ -1,8 +1,9 @@
-import { BaseEntity, Column, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 import { Client } from "./Client";
 import { Department } from "./Department";
 import { Employee } from "./Employee";
 import { Project_Category } from "./Project_Category";
+import { Project_file } from "./Project_File";
 
 
 
@@ -12,6 +13,14 @@ export enum enumCurrency {
     EUR = 'EUR',
     INR = 'INR',
     VND = 'VND',
+}
+
+export enum enumStatus {
+    NOT_STARTED = 'Not Started',
+    IN_PROGRESS = 'In Progress',
+    ON_HOLD = 'On Hold',
+    CANCELED = 'Canceled',
+    FINISHED = 'Finished',
 }
 
 @Entity()
@@ -28,10 +37,11 @@ export class Project extends BaseEntity {
     @Column({ type: 'date' })
     Deadline: Date
 
-    @Column()
+    @Column({nullable: true})
     project_summary: string
+    
 
-    @Column()
+    @Column({nullable: true})
     notes: string
 
     @Column({ nullable: true })
@@ -44,19 +54,22 @@ export class Project extends BaseEntity {
     send_task_noti: boolean
 
     @ManyToOne(() => Project_Category, (project_Category) => project_Category.projects, {
-        onDelete: 'SET NULL'
+        onDelete: 'SET NULL',
+        nullable: true
     })
     @JoinColumn()
     project_category: Project_Category
 
     @ManyToOne(() => Department, (department) => department.projects, {
-        onDelete: 'SET NULL'
+        onDelete: 'SET NULL',
+        nullable: true
     })
     @JoinColumn()
     department: Department
 
     @ManyToOne(() => Client, (client) => client.projects, {
-        onDelete: 'CASCADE'
+        onDelete: 'SET NULL',
+        nullable: true
     })
     @JoinColumn()
     client: Client
@@ -65,12 +78,36 @@ export class Project extends BaseEntity {
 	@JoinTable({ name: 'project_employee'})
 	employees: Employee[]
 
+    @Column({type: 'enum', enum: enumStatus, default: enumStatus.NOT_STARTED})
+    project_status: string
+
 
     @Column({ type: 'enum', enum: enumCurrency, default: enumCurrency.USD })
     currency: string
 
+    @Column({default: 0})
+    Progress: number
+    
 
+    @ManyToOne(() => Employee, (Employee) => Employee.projects, {
+        onDelete: 'SET NULL'
+    })
+    @JoinColumn()
+    Added_by: Employee
 
+    @OneToMany(() => Project_file, (project_file) => project_file.project,{
+        onDelete: 'SET NULL'
+    })
+    @JoinColumn()
+    project_files: Project_file[]
 
+	@CreateDateColumn({
+		name: 'created_at',
+	})
+	createdAt: Date
 
+	@UpdateDateColumn({
+		name: 'updated_at',
+	})
+	updatedAt: Date
 }
