03e484b8cc993c74eb3b1bc873c29862b8511059
nguyen quang hoang <hoanghpang@gmail.com>
2022-06-09 14:23:15 +0700

    update

--- build/index.js
+++ build/index.js
@@ -21,7 +21,7 @@ const httpServer = (0, http_1.createServer)(app);
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
-    origin: [`${process.env.URL_CLIENT}`],
+    origin: `${process.env.URL_CLIENT}`,
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false
