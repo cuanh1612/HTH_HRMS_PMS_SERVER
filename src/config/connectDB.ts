1efc0ad3c799648e482f229e10875df0bcaf67b5
nguyen quang hoang <hoanghpang@gmail.com>
2022-06-14 20:02:16 +0700

    create api  call video successfully

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -46,15 +46,19 @@ const connectDB = () => {
 		type: 'postgres',
 		name: 'huprom',
 		logging: true,
-		database: 'hth_hrms_pms',
-		password: 'kingspear1999',
-		username: 'postgres',
+
+		// database: 'hth_hrms_pms',
+		// password: 'kingspear1999',
+		// username: 'postgres',
+
 		synchronize: true,
 		port: 5432,
-		// url: `${process.env.DB_URL}`,
-		// ssl: {
-		// 	rejectUnauthorized: false
-		// },
+
+		url: `${process.env.DB_URL}`,
+		ssl: {
+			rejectUnauthorized: false
+		},
+
 		entities: [
 			Employee,
 			Avatar,
