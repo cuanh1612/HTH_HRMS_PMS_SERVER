9b1bbe592089cdcd5ebf66bb44ab42f646bb576c
nguyen quang hoang <hoanghpang@gmail.com>
2022-07-01 11:00:11 +0700

    update

--- src/config/socketIO.ts
+++ src/config/socketIO.ts
@@ -21,7 +21,7 @@ const createSocketServer = (httpServer: Server) => {
 
 	const io = new ServerSocket(httpServer, {
 		cors: {
-			origin: process.env.CLIENT_URL,
+			origin: 'https://huprom-hrms-pms-client.vercel.app',
 			methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 			credentials: true,
 		},
