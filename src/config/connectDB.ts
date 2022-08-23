32071a06b50350e15da26a0d4de5b8ebaf60c1b2
Nguyen Quang Hoang <hoangdev161201@gmail.com>
2022-08-23 07:36:09 +0700

    upgrade

--- src/config/connectDB.ts
+++ src/config/connectDB.ts
@@ -59,8 +59,8 @@ const connectDB = () => {
 		name: 'huprom',
 		logging: true,
 
-		database: 'HTH_HRMS_PMS',
-		password: '161201',
+		database: 'hth_hrms_pms',
+		password: 'Truong123456789@',
 
 		username: 'postgres',
 
--- src/routes/leaveRouter.ts
+++ src/routes/leaveRouter.ts
@@ -4,17 +4,17 @@ import { checkAuth } from '../utils/middleware/checkAuth'
 
 const leaveRouter = express.Router()
 
-leaveRouter.post('/', checkAuth(['Admin']), leaveController.create)
-leaveRouter.post('/delete-many', checkAuth(['Admin']), leaveController.deleteMany)
+leaveRouter.post('/', checkAuth(['Admin', 'Employee']), leaveController.create)
+leaveRouter.post('/delete-many', checkAuth(['Admin', 'Employee']), leaveController.deleteMany)
 
 leaveRouter.get('/', leaveController.getAll)
 leaveRouter.get('/:leaveId', leaveController.getDetail)
 
-leaveRouter.delete('/:leaveId', checkAuth(['Admin']), leaveController.delete)
+leaveRouter.delete('/:leaveId', checkAuth(['Admin', 'Employee']), leaveController.delete)
 
 leaveRouter.put('/:leaveId', checkAuth(['Admin']), leaveController.update)
 
-leaveRouter.put('/status/:leaveId', checkAuth(['Admin']), leaveController.updateStatus)
+leaveRouter.put('/status/:leaveId', checkAuth(['Admin', 'Employee']), leaveController.updateStatus)
 
 
 export default leaveRouter
