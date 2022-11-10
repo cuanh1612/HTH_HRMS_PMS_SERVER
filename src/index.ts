f2d0ba0744dd538cd9677490ab45f618f24af1fe
nguyen quang hoang <hoangdev161201@gmail.com>
2022-11-10 16:04:37 +0700

    upgrade

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.set('trust proxy', 1)
 app.use(
 	cors({
 		// 'https://huprom-hrms-pms-client.vercel.app'
-		origin: 'http://localhost:3000',
+		origin: 'https://huprom-hrms-pms-client.vercel.app',
 		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
 		credentials: true,		
 	})
