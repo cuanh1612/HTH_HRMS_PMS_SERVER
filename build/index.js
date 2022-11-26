ef4350b57941065a8e2e19d5cc80b80f285ddde0
nguyen quang hoang <hoangdev161201@gmail.com>
2022-11-26 11:43:36 +0700

    upgrade

--- build/index.js
+++ build/index.js
@@ -21,7 +21,7 @@ app.use(express_1.default.json());
 app.set('trust proxy', 1);
 app.use((0, cors_1.default)({
     // 'https://huprom-hrms-pms-client.vercel.app'
-    origin: 'http://localhost:3000',
+    origin: 'https://huprom-hrms-pms-client.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
 }));
--- src/routes/stickyNote.router.ts
+++ src/routes/stickyNote.router.ts
@@ -2,11 +2,9 @@ import express from 'express'
 import stickyNoteController from '../controllers/stickyNote.controller'
 import { checkAuth } from '../utils/middleware/checkAuth'
 
-
 const stickyNoteRouter = express.Router()
 
 stickyNoteRouter.post('/', checkAuth([]), stickyNoteController.create)
-
 
 stickyNoteRouter.put('/:id', checkAuth([]), stickyNoteController.update)
 
@@ -14,6 +12,5 @@ stickyNoteRouter.get('/:id', stickyNoteController.getDetail)
 stickyNoteRouter.get('/', stickyNoteController.getByEmployee)
 
 stickyNoteRouter.delete('/:id', checkAuth([]), stickyNoteController.delete)
-
 
 export default stickyNoteRouter
